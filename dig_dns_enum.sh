fa0034714b84ffb5bd847b53dddefcda945c3977
Hosam Gemei <5104207+Gemei@users.noreply.github.com>
2019-04-01 15:01:37 +0200

    Cleaned dos2unix

--- dig_dns_enum.sh
+++ dig_dns_enum.sh
@@ -6,7 +6,7 @@ if [ -z "$1" ];then
 	echo "[*] Simple Zone transfer script"
 	echo "[*] Usage   : $0 <domain name> "
 	exit 0
-fi 
+fi
 
 # if argument was given, identify the DNS servers for the domain
 results=$(for server in $(host -t ns $1 | cut -d" " -f4);do
